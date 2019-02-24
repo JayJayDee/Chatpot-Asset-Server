6aeb0f4d390928a33d2a7afc18462df94a41b31f
jaydee <jindongp@gmail.com>
2019-02-24 12:37:51 +0900

    added avatar-generator.

--- src/endpoints/avatar-endpoints.ts
+++ src/endpoints/avatar-endpoints.ts
@@ -2,6 +2,8 @@ import { injectable } from 'smart-factory';
 import { EndpointModules } from './modules';
 import { EndpointTypes } from './types';
 
+const AvatarGenerator = require('avatar-generator');
+
 injectable(EndpointModules.AvatarEndpoints.GET,
   [ EndpointModules.Utils.WrapAync ],
   async (wrapAsnc: EndpointTypes.Utils.WrapAsync): Promise<EndpointTypes.Endpoint> =>
@@ -10,7 +12,10 @@ injectable(EndpointModules.AvatarEndpoints.GET,
     uri: '/avatar',
     method: EndpointTypes.EndpointMethod.GET,
     handler: [
-      wrapAsnc((req, res, next) => {
+      wrapAsnc(async (req, res, next) => {
+        const av = new AvatarGenerator({ imageExtension: 'png' });
+        const image = await av.generate('viking swarp', 'female');
+        image.png().toFile('output.png');
         res.status(200).json({});
       })
     ]
