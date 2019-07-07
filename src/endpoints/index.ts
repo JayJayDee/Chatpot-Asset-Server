ec13cc86b8e304b472657232559084bed3d72181
JayJayDee <jindongp@gmail.com>
2019-07-07 14:20:30 +0900

    added meme endpoints. added swagger-ui.

--- src/endpoints/index.ts
+++ src/endpoints/index.ts
@@ -6,7 +6,17 @@ export { EndpointModules } from './modules';
 // register endpoints to container.
 injectable(EndpointModules.Endpoints,
   [ EndpointModules.ImageEndpoints.Upload,
-    EndpointModules.AvatarEndpoints.GET ],
+    EndpointModules.AvatarEndpoints.GET,
+    EndpointModules.MemeEndpoints.Upload,
+    EndpointModules.MemeEndpoints.Delete,
+    EndpointModules.MemeEndpoints.Memes ],
 
-  async (imageUpload, avatarGet) =>
-    ([ imageUpload, avatarGet ]));
+  async (imageUpload, avatarGet,
+    mupd, mdel, ms) =>
+    ([
+      imageUpload,
+      avatarGet,
+      mupd,
+      mdel,
+      ms
+    ]));
